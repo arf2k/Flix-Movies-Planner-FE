48f3dccdae4477f4a475e4bec0f4064b3faf8dd9
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-09-28 13:33:10 -0400

    checkbox array collected

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 document.addEventListener('DOMContentLoaded', e => {
 
-   function getCheckboxValues(scene){
-     const checkboxes = document.querySelectorAll(`input[name="${scene}"]:checked`);
-     let values = []
-     checkboxes.forEach((checkbox){ 
-     values.push(checkbox.value);
-   }); 
-   }
+//    function getCheckboxValues(scene){
+//      const checkboxes = document.querySelectorAll(`input[name="${scene}"]:checked`);
+//      let values = []
+//      checkboxes.forEach((checkbox) => { 
+//      values.push(checkbox.value);
+//    }); 
+//      return values 
+//    }
 
-const checkBoxForm = document.querySelector()
+const checkBoxForm = document.querySelector("#checkboxform")
+const formCheck = document.querySelector('.form-check-label')
 checkBoxForm.addEventListener('submit', e => {
-     getCheckboxValues()
-     return getCheckboxValues
+     e.preventDefault()
+//      checkBoxForm.querySelectorAll("input").forEach(function(input) {
+//        if(input.value === "on") {
+//         console.log(formCheck.value)
+
+     let inputs = checkBoxForm.children
+     let array = [] 
+     for (let i = 0; i <inputs.length; i++){
+          if(inputs[i].checked)
+          array.push(inputs[i].value)
+     }  
+     console.log(array) 
+})
+//        }
+//      })
+//      })
+//     })
+  
+
+    
+// const renderChosenScenes = () => {
+//   const sceneContainer = document.querySelector('#scene-container')
+//   const addLi = document.createElement('li')
+// // do a loop on each scene selected and display the scenes
+// // create a delete button 
+  
+  
+// }
+
+
 })
 
-
-
-
-
-
-
-
-
-
-
-
-})
