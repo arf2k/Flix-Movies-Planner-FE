29bed23c6bf208d5444fd59e5b1fd529b75f9baa
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-09-28 17:19:26 -0400

    lots of frontend changes

--- index.js
+++ index.js
@@ -1,45 +1,88 @@
 document.addEventListener('DOMContentLoaded', e => {
 
-//    function getCheckboxValues(scene){
-//      const checkboxes = document.querySelectorAll(`input[name="${scene}"]:checked`);
-//      let values = []
-//      checkboxes.forEach((checkbox) => { 
-//      values.push(checkbox.value);
-//    }); 
-//      return values 
-//    }
+settingsUrl = 'http://localhost:3000/settings/'
+addressesUrl = 'http://localhost:300/locations/'
+
+const getSettings = () => {
+     fetch(settingsUrl)
+     .then(res => res.json())
+     .then(data => {
+          renderSettings(data)
+})
+}
+
+const renderSettings = data => {
+     for(let setting of data) {
+          renderSetting(setting) 
+     }
+}
+
+const renderSetting = setting => {
+     const settingsContainer = document.querySelector("#settings-container")
+     const settingP = document.createElement('p')
+     const settingImg = document.createElement('img')
+     // settingImg.src = setting.image_url
+     settingP.dataset.id = setting.id
+     settingP.textContent = setting.name
+     settingP.addEventListener('click', onSettingClick)
+     settingsContainer.append(settingP)
+     // append settingImg once we have image container
+}
+
+function onSettingClick(e){
+     
+     getAddresses(e.target.dataset.id)
+}
+
+const getAddresses = () => {
+     fetch(addressesUrl)
+     .then(res => res.json())
+     .then(data => {
+          renderAddresses(data)
+     })
+}
+
+const renderAddresses = data => {
+     for(let address of data) {
+          renderAddress(address)
+     }
+}
+
+const renderAddress = address => {
+     
+}
+
+
 
 const checkBoxForm = document.querySelector("#checkboxform")
 const formCheck = document.querySelector('.form-check-label')
 checkBoxForm.addEventListener('submit', e => {
      e.preventDefault()
-//      checkBoxForm.querySelectorAll("input").forEach(function(input) {
-//        if(input.value === "on") {
-//         console.log(formCheck.value)
-
      let inputs = checkBoxForm.children
      let array = [] 
      for (let i = 0; i <inputs.length; i++){
           if(inputs[i].checked)
           array.push(inputs[i].value)
      }  
-     console.log(array) 
+     renderChosenScenes(array) 
 })
-//        }
-//      })
-//      })
-//     })
-  
+
 
     
-// const renderChosenScenes = () => {
-//   const sceneContainer = document.querySelector('#scene-container')
-//   const addLi = document.createElement('li')
-// // do a loop on each scene selected and display the scenes
-// // create a delete button 
+function renderChosenScenes(scenes){
+  const sceneContainer = document.querySelector('#scene-container')
+  console.log(scenes)
+  for(let scene of scenes){
+     const addLi = document.createElement('li')
+     addLi.textContent = scene 
+     sceneContainer.append(addLi) 
+  }
+}
   
-  
-// }
+
+
+getSettings()
+getAddresses()
 
 
 })
